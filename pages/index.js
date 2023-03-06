6008cf2e9f2a4ebeb8cea63dab1c57093a627888
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2023-03-06 12:40:35 +0800

    change text

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            You wont believe how lifechanging this is &nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
